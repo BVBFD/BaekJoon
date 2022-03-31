a3c93fca2215bb50aa6c814b5f9bce2c8e9375ef
BVBFD <lsevina126@gmail.com>
2022-03-31 22:55:01 +0900

    2438, 2739 questions solved

--- star_1.py
+++
@@ -1,6 +0,0 @@
-a = int(input("반복횟수를 입력하세요 : "))
-
-for i in range(a):
-    for n in range(i+1):
-        print("*", end="")
-    print("")
--- star_3.py
+++
@@ -1,6 +0,0 @@
-a = int(input("반복 횟수를 입력하세요 : "))
-
-for i in range(a, 0, -1):
-    for n in range(i):
-        print("*", end="")
-    print("")
